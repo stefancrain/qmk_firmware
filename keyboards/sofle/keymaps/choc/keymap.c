e197c808b3534c5c7367b17d2b3d42f5d7f1537c
Stefan Crain <stefancrain@gmail.com>
2022-02-08 15:54:27 -0500

    Reverse rotary encoder volume controll, CW = Increase, CCW = Decrease

--- keyboards/sofle/keymaps/choc/keymap.c
+++ keyboards/sofle/keymaps/choc/keymap.c
@@ -437,9 +437,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_VOLD);
-        } else {
             tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
         }
     } else if (index == 1) {
         if (clockwise) {
